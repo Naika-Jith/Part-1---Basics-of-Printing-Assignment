dafd9c046d0c44fe1bbfc76abc627a1cf7d82dc0
Naika Jith <naika.jith@outlook.com>
2026-09-10 13:19:44 -0400

    Mostly Done!!!

--- Part 1 - Basics of Printing Assignment/Program.cs
+++ Part 1 - Basics of Printing Assignment/Program.cs
@@ -10,16 +10,29 @@ namespace Part_1___Basics_of_Printing_Assignment
     {
         static void Main(string[] args)
         {
+            Console.Title = ("Basics Of Printing Assignment");
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Welcome to the Basics of Printing Assignment");
+            Console.WriteLine();
 
+            string firstName = "Naika";
+            string favMovie = " The Matrix";
 
+            Console.WriteLine($"hey there, my name is {firstName} and " +
+                $"my favourite movie is the {favMovie}");
 
+            Console.WriteLine(firstName.ToLower());
+            Console.WriteLine(favMovie.ToLower());
 
+            Console.WriteLine(favMovie.ToUpper());
+            Console.WriteLine($"{favMovie}");
 
+            Console.WriteLine(favMovie.Contains("THE"));
 
-
-
-
-
+            favMovie = favMovie.Replace("A", "@");
+            favMovie = favMovie.Replace("E", "3");
+            Console.WriteLine(favMovie);
         }
     }
 }
