896a5a153532cae220afec35b3e96a0af44de2ed
Naika Jith <naika.jith@outlook.com>
2026-09-17 12:04:12 -0400

    Mostly All Done!!!!

--- Part 1 - Basics of Printing Assignment/Program.cs
+++ Part 1 - Basics of Printing Assignment/Program.cs
@@ -11,28 +11,36 @@ namespace Part_1___Basics_of_Printing_Assignment
         static void Main(string[] args)
         {
             Console.Title = ("Basics Of Printing Assignment");
+            Console.WriteLine("Please press ENTER to continue");
             Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Welcome to the Basics of Printing Assignment");
             Console.WriteLine();
 
             string firstName = "Naika";
-            string favMovie = " The Matrix";
+            string favMovie = "The Matrix";
 
-            Console.WriteLine($"hey there, my name is {firstName} and " +
-                $"my favourite movie is the {favMovie}");
 
-            Console.WriteLine(firstName.ToLower());
-            Console.WriteLine(favMovie.ToLower());
+            Console.WriteLine($"hey there, my name is {firstName.ToLower()} and my favourite movie is {favMovie.ToLower()}");
+            Console.WriteLine();
 
-            Console.WriteLine(favMovie.ToUpper());
-            Console.WriteLine($"{favMovie}");
 
-            Console.WriteLine(favMovie.Contains("THE"));
+            favMovie = favMovie.ToUpper();
+            Console.WriteLine(favMovie);
+            Console.WriteLine();
+           Console.WriteLine(favMovie.Contains("THE"));
+            Console.WriteLine();
 
             favMovie = favMovie.Replace("A", "@");
             favMovie = favMovie.Replace("E", "3");
             Console.WriteLine(favMovie);
+
+            //ASCII ART
+
+            Console.WriteLine(" __         __\r\n/  \\.-\"\"\"-./  \\\r\n\\    -   -    /\r\n |   o   o   |\r\n \\  .-'''-.  /\r\n  '-\\__Y__/-'\r\n     `---`");
+            Console.Write("((_,...,_))\r\n   |o o|\r\n   \\   /\r\n    ^_^   ");
+
         }
     }
 }
